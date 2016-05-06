5a14652d08e621eaf0772065e5627cd276d66003
mustaric <mustaric@gmail.com>
2016-05-05 17:21:15 -0700

    Got the justVet script to work

--- runbackend/justVetK2.py
+++ runbackend/justVetK2.py
@@ -21,9 +21,13 @@ import numpy as np
 import sys
 import getopt as getopt
 import dave.pipeline.exporter as ex
+import dave.pipeline.multiPagePlot as mpp
+import datetime
+import dave.pipeline.pipeline as dpp
 
 def main():
     """A bare bones main program"""
+    
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hf:c:o:", ["help", "output=","config=","file="])
@@ -39,16 +43,16 @@ def main():
     for o, a in opts:
         if o in ("-f","--file"):
             ephemFile = a
-            print "Ephemeeris File is: %s" % ephemFile
+            print "Ephemeris File is: %s" % ephemFile
         elif o in ("-h", "--help"):
             usage()
             sys.exit()
         elif o in ("-o", "--output"):
             output = a
-            print output
+            print "Output File is %s\n" % output
         elif o in ("-c", "--config"):
             cfgFile= a
-            print "Config File is: %s" % cfgFile
+            print "Config File is: %s\n" % cfgFile
         else:
             assert False, "Unhandled option"
             sys.exit()
@@ -56,7 +60,7 @@ def main():
     cfg=loadConfigInput(cfgFile)
      
     cfg=suppConfiguration(cfg)
-    print cfg 
+    #print cfg 
     
     data=loadEphemFile(ephemFile)
     
@@ -66,11 +70,20 @@ def main():
             dep=data[i,4]/1.0e6
         except:
             dep=.00005
+           
+        clip=runOneEphem(epic,data[i,2],data[i,3],cfg,duration=3.0,depth=dep) 
+        
+        print clip.__meta__
+        
+        if ('exception' not in clip.keys()):
+            outfile=runExport(clip,output)
+            print 'Created Outputs %s\n\n' % outfile
+        else:
+            print "No Outputs\n"
+            #outfile=runExport(clip,output)
+            print clip.exception
+            print clip.backtrace
             
-        clip=runOneEphem(epic,data[i,2],data[i,3],cfg,duration=2,depth=dep)    
-        runExport(clip,output)
-    
-
  
 
 def usage():
@@ -93,7 +106,7 @@ def loadEphemFile(ephemFile):
     """
     
     data=np.loadtxt(ephemFile,dtype=float,comments='#',delimiter=None)
-    
+    print "Loaded %s\n" % ephemFile
 
     return data    
     
@@ -109,7 +122,10 @@ def loadConfigInput(cfgFile):
     info=np.loadtxt(cfgFile,dtype=str,delimiter=":",comments='#')
     
     for i,key in enumerate(info[:,0]):
-        cfg[key]=info[i,1]
+        try:
+            cfg[key]=float(info[i,1])
+        except ValueError:
+            cfg[key]=info[i,1]
     
     return cfg
    
@@ -134,7 +150,12 @@ def suppConfiguration(cfg):
     measureDiffImgCentroidsTask dispositionTask""".split()
 
     cfg['searchTaskList'] = searchTaskList
-
+    try:
+        cfg['timeout_sec'] =  int(cfg['timeout_sec'])
+    except:
+        cfg['timeout_sec'] = 150
+        
+        
     return cfg
 
 
@@ -186,10 +207,30 @@ def runExport(clip,output):
     run the exporters based on the input clip.
     Append the important information to the output File.
     """
-    outstr=ex.createExportString(clip, delimiter=" ", badValue="nan"):
+    per=np.round(clip.bls.period*10)
+    epoch=np.round(clip.bls.epoch)
     
+        
+    outstr,header=ex.createExportString(clip, delimiter=" ", badValue="nan")
+    print outstr
+
+    fid=open(output,'a')
+    fid.write(outstr)
+    fid.close()    
+
+    tag="%i-%02i-%04i" % (clip.value,per,epoch)
+    outfile="jvet%s.pdf" % (tag)
+    date=datetime.datetime.now()
     
-    
+    if ('disposition' not in clip.keys()):
+        clip['disposition'] = 'No Disposition Determined'
+        clip.disposition.isCandidate = 0
+        clip.disposition.isSignificantEvent = 0
+        
+    mpp.plot_multipages(outfile, clip, date)
+
+        
+    return outfile
     
 
 if __name__ == "__main__":
