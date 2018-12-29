e5e97f170d2417b58d9487a2f0e7551df0c699cb
Fergal <fergal.mullally@gmail.com>
2018-12-29 16:10:03 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+#This is a minimal setup.py that I use when an OI package is missing one.
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Dave',
+    url = 'https://github.com/exoplanetvetting/DAVE',
+    version = '0.1.0',
+    author = 'The Dave Team',
+    author_email = 'author@gmail.com',
+    description = 'Detection and Vetting of Exoplanets',
+
+    #packages=find_packages('./src/py', exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    package_dir={'': 'dave'},
+)
