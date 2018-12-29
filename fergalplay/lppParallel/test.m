06c4b2e2531d2d61e6163e24b1251ab8a788e1d0
Fergal <fergal.mullally@gmail.com>
2018-12-29 16:30:01 -0500

    deleting

--- fergalplay/lppParallel/test.m
+++
@@ -1,9 +0,0 @@
-
-function sum = testFunc()
-%A test function that does some kind of computation
-sum = 0
-for i = 1:1e3
-    for j = 1:e3
-        sum = sum + 1
-    end
-end    
--- fileio/pyfits.py
+++
@@ -1,5 +0,0 @@
-#Much of fergal's code imports pyfits. So this module stops python
-#complaining because pyfits doesn't exist anymore.
-
-from astropy.io.fits import *
-
