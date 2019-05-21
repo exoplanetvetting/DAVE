76b93a799eee09c627e7154b53eeaaa34072ea5c
vbkostov <veselin.b.kostov@gmail.com>
2019-05-21 15:45:07 -0400

    Update runOneTESS.py

--- runOneTESS.py
+++ runOneTESS.py
@@ -4,9 +4,10 @@
 from dave.tessPipeline import vet_tess_
 
 detrendType_ = "tess_2min"#"eleanor"#
+sector_ = 2
 
 # Input is: Sector, TIC ID, Planet Number, Period, BTJD, Transit Depth [ppm], Transit Duration [hours]
-clip = vet_tess_.runOneDv(detrendType_, 307210830, 1, 3.690613, 1356.2038, 1863, 1.27)
+clip = vet_tess_.runOneDv(detrendType_, sector_, 307210830, 1, 3.690613, 1356.2038, 1863, 1.27)
 #clip = vet_tess_.runOneDv(detrendType_, 1, 271893367,1,5.8707,1326.2738,5338.0,2.4)
 
 outfile_ = 'tmp.txt'
